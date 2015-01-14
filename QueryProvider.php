8e6dda71639ab7fee0d839e7c32d6bf34ac1883b
Tom Anderson <tom.h.anderson@gmail.com>
2015-01-14 16:07:56 -0700

    asdf

--- QueryProvider.php
+++ QueryProvider.php
@@ -2,49 +2,15 @@
 
 namespace Db\Query\Provider;
 
-use Zend\ServiceManager\ServiceLocatorAwareInterface;
-use Zend\ServiceManager\ServiceLocatorInterface;
 use ZF\Apigility\Doctrine\Server\Collection\Query\FetchAllOrmQuery;
 
-/**
- * Helper for fetching ContentManagement Pages
- */
-class DataPointQueryProvider extends FetchAllOrmQuery implements ServiceLocatorAwareInterface
+class DataPointQueryProvider extends FetchAllOrmQuery
 {
-    /**
-     * @var ServiceLocatorInterface
-     */
-    protected $serviceLocator = null;
-
-    /**
-     * Set service locator
-     *
-     * @param ServiceLocatorInterface $serviceLocator
-     * @return mixed
-     */
-    public function setServiceLocator(ServiceLocatorInterface $serviceLocator)
-    {
-        $this->serviceLocator = $serviceLocator;
-
-        return $this;
-    }
-
-    /**
-     * Get service locator
-     *
-     * @return ServiceLocatorInterface
-     */
-    public function getServiceLocator()
-    {
-        return $this->serviceLocator;
-    }
-
     /**
      * Create a filtered query with required parameters
      */
     public function createQuery($entityClass, $parameters)
     {
-        $objectManager = $this->getServiceLocator()->getServiceLocator()->get('doctrine.entitymanager.orm_default');
         $queryBuilder = $this->getObjectManager()->createQueryBuilder();
 
         $queryBuilder->select('row')
